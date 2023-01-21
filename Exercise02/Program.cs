76e940d0ab8cb7abe5929f8e51675835a6e031cf
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-22 00:28:22 +0300

    Задание 2. Выполнено

--- Exercise02/Program.cs
+++ Exercise02/Program.cs
@@ -7,3 +7,39 @@
 Написать программу, которая по заданному номеру четверти плоскости
 показывает возможный диапазон координат в этой четверти.
 */
+
+int CoordinatesX(int number){
+    if ((number == 1) || (number == 4))
+        return 1;
+    else if ((number == 2) || (number == 3))
+        return -1;
+    return 0;
+}
+
+int CoordinatesY(int number){
+    if ((number == 1) || (number == 2))
+        return 1;
+    else if ((number == 3) || (number == 4))
+        return -1;
+    return 0;
+}
+
+Console.WriteLine("Программа показывает возможный диапазон координат по введённому номеру четверти плоскости.");
+Console.Write("Введите номер четверти плоскости: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num != 1) || (num != 2) || (num != 3) || (num != 4)){
+    Console.WriteLine($"Введённого номера плоскости {num} не существует. Программа прервана");
+    return;
+}
+
+int x = CoordinatesX(num);
+int y = CoordinatesY(num);
+
+if ((x == 1) && (y == 1))
+    Console.WriteLine($"Координаты {num} четверти: X от {x} до плюс бесконечности, Y от {y} до плюс бесконечности");
+else if ((x == -1) && (y == 1))
+    Console.WriteLine($"Координаты {num} четверти: X от {x} до минус бесконечности, Y от {y} до плюс бесконечности");
+else if ((x == -1) && (y == -1))
+    Console.WriteLine($"Координаты {num} четверти: X от {x} до минус бесконечности, Y от {y} до минус бесконечности");
+else if ((x == 1) && (y == -1))
+    Console.WriteLine($"Координаты {num} четверти: X от {x} до плюс бесконечности, Y от {y} до минус бесконечности");
